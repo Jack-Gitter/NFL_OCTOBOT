c7723936b588a65f06e11f431437bed198c1af7b
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 16:36:50 -0400

    nice

---
+++ src/coordinator/coordinator.ts
@@ -0,0 +1,32 @@
+import { getAtheleteInformation, getDailyGameIds, getGameScoringPlayIds, getOctopusInformation } from "../espn_api/espn_api"
+import { AthleteAndOctopusInformation } from "../espn_api/types"
+import { getTwitterClient, postOctopus } from "../x_api/x_api"
+
+export const checkForOctopus = async () => {
+    const twitterClient = await getTwitterClient()
+    const gameIds = await getDailyGameIds(new Date('09/29/2024'))
+    const gameToScoringPlayIdsArray = await Promise.all(gameIds.map(async (gameId: number) => {
+        return await getGameScoringPlayIds(gameId)
+    }))
+
+    await Promise.all (gameToScoringPlayIdsArray.map(async (gameToScoringPlayIds) => {
+        const scoringPlayIds = gameToScoringPlayIds.scoringPlayIds
+        const octopusInformationArray = await Promise.all(scoringPlayIds.map(async (scoringPlayId) => {
+            return await getOctopusInformation(gameToScoringPlayIds.gameId, scoringPlayId)
+        }))
+        const athleteAndOctopusInformationArray = await Promise.all(octopusInformationArray.map(async (octopusInformation) => {
+            if (octopusInformation) {
+                const athlete = await getAtheleteInformation(octopusInformation.scorer)
+                return {
+                    athlete, 
+                    octopusInformation
+                } satisfies AthleteAndOctopusInformation
+            }
+        }))
+        for (const athleteAndOctopusInformation of athleteAndOctopusInformationArray) {
+            if (athleteAndOctopusInformation?.athlete && athleteAndOctopusInformation.octopusInformation) {
+                postOctopus(twitterClient, athleteAndOctopusInformation?.octopusInformation.shortText)
+            }
+        }
+    }))
+}
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,13 @@
 import { configDotenv } from "dotenv"
-import { getAtheleteInformation, getDailyGameIds, getGameScoringPlayIds, getOctopusInformation, } from "./espn_api/espn_api"
-import { AthleteAndOctopusInformation } from "./espn_api/types"
-import { getTwitterClient, postOctopus } from "./x_api/x_api"
+import cron from 'node-cron'
+import { checkForOctopus } from "./coordinator/coordinator"
 
 const main = async () => {
     configDotenv()
-    const twitterClient = await getTwitterClient()
-    const gameIds = await getDailyGameIds(new Date('09/29/2024'))
-    const gameToScoringPlayIdsArray = await Promise.all(gameIds.map(async (gameId: number) => {
-        return await getGameScoringPlayIds(gameId)
-    }))
-
-    await Promise.all (gameToScoringPlayIdsArray.map(async (gameToScoringPlayIds) => {
-        const scoringPlayIds = gameToScoringPlayIds.scoringPlayIds
-        const octopusInformationArray = await Promise.all(scoringPlayIds.map(async (scoringPlayId) => {
-            return await getOctopusInformation(gameToScoringPlayIds.gameId, scoringPlayId)
-        }))
-        const athleteAndOctopusInformationArray = await Promise.all(octopusInformationArray.map(async (octopusInformation) => {
-            if (octopusInformation) {
-                const athlete = await getAtheleteInformation(octopusInformation.scorer)
-                return {
-                    athlete, 
-                    octopusInformation
-                } satisfies AthleteAndOctopusInformation
-            }
-        }))
-        for (const athleteAndOctopusInformation of athleteAndOctopusInformationArray) {
-            if (athleteAndOctopusInformation?.athlete && athleteAndOctopusInformation.octopusInformation) {
-                postOctopus(twitterClient, athleteAndOctopusInformation?.octopusInformation.shortText)
-            }
-        }
-    }))
-
+    cron.schedule('* * * * *', () => {
+        checkForOctopus()
+    })
 }
+
 
 main()
