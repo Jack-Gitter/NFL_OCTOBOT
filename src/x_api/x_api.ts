d9c03c6e34ec29445a72e198e03098502e933da2
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 12:27:47 -0400

    nice

--- src/x_api/x_api.ts
+++ src/x_api/x_api.ts
@@ -1,15 +1,10 @@
-const baseurl = `https://api.x.com/2/tweets`
+import { Client, auth } from "twitter-api-sdk";
+ 
+export const authClient = new auth.OAuth2User({
+ client_id: process.env.CLIENT_ID as string,
+ client_secret: process.env.CLIENT_SECRET as string,
+ callback: "YOUR-CALLBACK",
+ scopes: ["tweet.read", "users.read", "offline.access"],
+});
 
-export const post = async () => {
-    const res  = await fetch(baseurl, {
-        method: 'POST',
-        headers: {'content-type': 'application/json', 'authorization': `Bearer ${process.env.X_BEARER_TOKEN}`},
-        body: JSON.stringify({
-            text: 'testing!'
-        })
-    })
-
-    const data = await res.json()
-    console.log(data)
-}
-
+export const xClient = new Client(authClient);
