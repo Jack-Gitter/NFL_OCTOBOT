5f6b0e7e7ff76635325f93621349497c747d2b1b
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 12:19:16 -0400

    nice

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,14 +1,17 @@
-const express = require('express')
+import { xAuthClient } from "../x_api/x_api";
+import express from 'express'
+
 
 const app = express()
 const port = 3000
 const STATE = "my-state";
 
-app.get('/x_callback', (req, res) => { 
+app.get('/x_callback', async (req, res) => { 
+try {
 try {
     const { code, state } = req.query;
     if (state !== STATE) return res.status(500).send("State isn't matching");
-    await authClient.requestAccessToken(code);
+    await xAuthClient.requestAccessToken(code);
     res.redirect("/tweets");
   } catch (error) {
     console.log(error);
--- src/x_api/x_api.ts
+++ src/x_api/x_api.ts
@@ -1,20 +1,16 @@
 import { Client, auth } from "twitter-api-sdk";
 
-export const initXClient = () => {
-    const creds = new auth.OAuth2User({
-     client_id: process.env.X_API_KEY as string,
-     client_secret: process.env.X_SECRET_KEY as string,
-     callback: "",
-     scopes: [],
-    });
-    return new Client(creds)
-}
-
 export const post = async (client: Client, text: string) => {
     const body = {text}
     await client.tweets.createTweet(body)
 }
 
+export const xAuthClient = new auth.OAuth2User({
+ client_id: process.env.X_API_KEY as string,
+ client_secret: process.env.X_SECRET_KEY as string,
+ callback: "",
+ scopes: [],
+});
 
-export const xClient = initXClient()
+export const xClient = new Client(xAuthClient)
 
