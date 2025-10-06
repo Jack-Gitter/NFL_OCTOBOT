9648bde1c9db1b84f34491a53aa73f1f4921854b
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 17:41:09 -0400

    nice

--- src/datasource/datasource.ts
+++ src/datasource/datasource.ts
@@ -9,7 +9,7 @@ configDotenv()
 export default new DataSource({
     type: "postgres",
     host: "localhost",
-    port: 5433,
+    port: 5432,
     username: process.env.PG_USER as string,
     password: process.env.PG_PASS as string,
     database: process.env.PG_DB as string,
