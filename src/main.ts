dfe33cdeae7547ad2b1e2e357c0bf0fabf20ba99
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 10:52:33 -0400

    nice

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { getAtheleteInformation, getDailyGameIds, getGameTouchdownPlayIds, getOctopusInformation, } from "./espn_api/espn_api"
 
 const main = async () => {
-    const gameIds = await getDailyGameIds(new Date('12/31/2017'))
+    const gameIds = await getDailyGameIds(new Date('10/05/2025'))
     for (const gameId of gameIds) {
         const tdPlayIds = await getGameTouchdownPlayIds(gameId)
         for (const tdPlayId of tdPlayIds) {
