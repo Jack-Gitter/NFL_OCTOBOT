76dd5b1c1142e739a8a8dd24fd9a24796cac48bd
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 19:45:23 -0400

    nice

--- src/espn_api/espn_api.ts
+++ src/espn_api/espn_api.ts
@@ -1,5 +1,5 @@
 import { Game } from "../models/game"
-import { Athlete, Event, Game, GameResponse, GameToScoringPlayIds, OctopusInformation, Participant, Scoreboard, SCORER_TYPE, SCORING_TYPE, ScoringPlay, ScoringPlayInformation } from "./types"
+import { Athlete, Event, GameResponse, Scoreboard, ScoringPlay, ScoringPlayInformation } from "./types"
 
 export const getScoringPlayInformation = async (gameId: number, scoringPlayIds: number[]) => {
     return await Promise.all(scoringPlayIds.map(async (scoringPlayId) => {
@@ -28,7 +28,6 @@ export const getGameInformation = async (gameId: number) => {
 
 }
 
-
 export const getDailyGameIds = async (date: Date = new Date()) => {
     const formattedDate = formatDate(date)
     const url = `https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates=${formattedDate}`
@@ -37,30 +36,6 @@ export const getDailyGameIds = async (date: Date = new Date()) => {
     return scoreboard.events.map((event: Event) => {
         return event.id
     })
-}
-
-export const getOctopusInformation = async(gameId: number, scoringPlayId: number) => {
-    const url = `https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/${gameId}/competitions/${gameId}/plays/${scoringPlayId}`
-    const result = await fetch(url)
-    const scoringPlayInformation: ScoringPlayInformation = await result.json()
-    const isTwoPointConversion = scoringPlayInformation?.pointAfterAttempt?.value === 2 || (scoringPlayInformation?.text?.includes('TWO-POINT CONVERSION ATTEMPT') && scoringPlayInformation?.text?.includes('ATTEMPT SUCCEEDS'))
-    let patScorer = undefined
-    let tdScorer = undefined 
-    if (isTwoPointConversion) {
-        patScorer = scoringPlayInformation.participants.find((participant: Participant) => {
-            return participant.type === SCORER_TYPE.PAT_SCORER
-        })
-        tdScorer = scoringPlayInformation.participants.find((participant: Participant) => {
-            return participant.type === SCORER_TYPE.TD_SCORER
-        })
-    }
-    if (patScorer && tdScorer && patScorer.athlete.$ref === tdScorer.athlete.$ref) {
-        return {
-            scorer: patScorer.athlete.$ref,
-            shortText: scoringPlayInformation.shortText,
-            scoringPlayId: scoringPlayId
-        } satisfies OctopusInformation
-    }
 }
 
 export const getAtheleteInformation = async(playerUrl: string) => {
