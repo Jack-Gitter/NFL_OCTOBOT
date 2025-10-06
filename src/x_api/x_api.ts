a3bd84bd4136ac496c25065981d95bbb04bb045d
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 14:38:29 -0400

    nice

--- src/x_api/x_api.ts
+++ src/x_api/x_api.ts
@@ -1,14 +1,13 @@
 import { TwitterApi } from 'twitter-api-v2';
 
+export const twitterBaseUrl = `https://api.x.com/2/`
 export const post = async () => {
-    const url = `https://api.x.com/2/tweets`
 
-    const userClient = new TwitterApi({
+    const twitterClient = new TwitterApi({
       appKey: process.env.X_API_KEY as string,
       appSecret: process.env.X_API_KEY_SECRET as string,
       accessToken: process.env.X_ACCESS_TOKEN as string,
       accessSecret: process.env.X_ACCESS_TOKEN_SECRET as string
     });
 
-    userClient.post(url, {text: 'hi!'})
 }
