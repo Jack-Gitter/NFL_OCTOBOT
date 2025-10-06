2c0aed4aa1e857413955555b30ef4f3c8310b250
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 12:06:53 -0400

    ok

--- src/x_api/x_api.ts
+++ src/x_api/x_api.ts
@@ -1,7 +1,12 @@
 import { Client, auth } from "twitter-api-sdk";
 
 export const initXClient = () => {
-    const creds = new auth.OAuth2Bearer(process.env.X_BEARER_TOKEN ?? '')
+    const creds = new auth.OAuth2User({
+     client_id: process.env.CLIENT_ID as string,
+     client_secret: process.env.CLIENT_SECRET as string,
+     callback: "",
+     scopes: [],
+    });
     return new Client(creds)
 }
 
