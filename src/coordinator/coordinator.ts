859908f626fc75d468857319893d02c3d2cb4c8d
Jack Gitter <jack.gitter@gmail.com>
2025-10-06 17:12:32 -0400

    nice

--- src/coordinator/coordinator.ts
+++ src/coordinator/coordinator.ts
@@ -1,3 +1,5 @@
+import datasource from "../datasource/datasource"
+import { Play } from "../entities/Play"
 import { getAtheleteInformation, getDailyGameIds, getGameScoringPlayIds, getOctopusInformation } from "../espn_api/espn_api"
 import { AthleteAndOctopusInformation } from "../espn_api/types"
 import { getTwitterClient, postOctopus } from "../x_api/x_api"
@@ -5,9 +7,20 @@ import { getTwitterClient, postOctopus } from "../x_api/x_api"
 export const checkForOctopus = async () => {
     const twitterClient = await getTwitterClient()
     const gameIds = await getDailyGameIds(new Date('09/29/2024'))
+    const playRepository = datasource.getRepository(Play)
+    const checkedPlays = await playRepository.find()
+    const checkedPlayIds = checkedPlays.map(checkedPlay => {
+        return checkedPlay.id
+    })
+    const checkedPlayIdsSet = new Set(checkedPlayIds)
     const gameToScoringPlayIdsArray = await Promise.all(gameIds.map(async (gameId: number) => {
         return await getGameScoringPlayIds(gameId)
     }))
+    gameToScoringPlayIdsArray.forEach((gameToScoringPlayIds) => {
+        gameToScoringPlayIds.scoringPlayIds = gameToScoringPlayIds.scoringPlayIds.filter(scoringPlayId => {
+            return !checkedPlayIdsSet.has(scoringPlayId)
+        })
+    })
 
     await Promise.all (gameToScoringPlayIdsArray.map(async (gameToScoringPlayIds) => {
         const scoringPlayIds = gameToScoringPlayIds.scoringPlayIds
