83f0a15fdba1f69cfbfab61c0cde0f3e5be63a3b
Jack Gitter <jack.gitter@gmail.com>
2025-10-12 10:57:43 -0400

    nice

--- src/main.ts
+++ src/main.ts
@@ -63,9 +63,9 @@ const main = async () => {
       const highestMonthly = await getHighestMonthlyDonator(datasource);
       const totalMonthlyDonations = await getMonthlyDonationCount(datasource)
 
-      console.log(highestAllTime)
-      console.log(highestMonthly)
-      console.log(totalMonthlyDonations)
+      console.log(`highest all time: ${highestAllTime}`)
+      console.log(`highest monthly: ${highestMonthly}`)
+      console.log(`total monthly: ${totalMonthlyDonations}`)
 
       console.log('🏆 Highest All-Time Donator:', highestAllTime);
       console.log('🗓️ Highest Monthly Donator:', highestMonthly);
